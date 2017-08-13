242bd9dc126ed0d723bbba4f80efd8a49c5a4825
f321 <filip.lapovo@gmail.com>
2017-08-13 16:09:15 +0200

    asd

--- spring-guru/src/main/java/com/jdbcexample/MainApp.java
+++ spring-guru/src/main/java/com/jdbcexample/MainApp.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainApp {
 
 	public static void main(String[] args) {
-		//asdsaddffdgfdgfdgfd
 		SpringApplication.run(MainApp.class, args);
 	}
 
